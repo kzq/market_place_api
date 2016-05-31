05e799c9311e07d66900a157425415ee80249189
kzq <kami_ravian@yahoo.com>
2016-05-31 21:44:00 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  #api.market_place_api.dev
+  #using namespace directory remains the part of the url but with constrainst now its 
+  #a subdomin
   namespace :api, defaults: {format: :json}, constraints: { subdomain: 'api', path: '/'} do
-    
+    #api.market_place_api.dev/products
+    #using module directory skipped in path of resorce=s under directory
+    scope module: :v1 do
+      
+    end
   end
 end
